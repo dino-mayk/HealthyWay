50cd49a5c509dbf7aabf8c643a0df2d44bd898f9
Nikita Krasnov <nikitosikrasnov@gmail.com>
2023-11-11 21:08:22 +0300

    db update

--- HealthyWay/settings.py
+++ HealthyWay/settings.py
@@ -64,20 +64,10 @@ WSGI_APPLICATION = 'HealthyWay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase'
-    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
